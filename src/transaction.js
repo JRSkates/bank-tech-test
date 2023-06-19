e460023be771f52a8a65d40c7917ce850ebde182
JRSkates <skatesproduction@gmail.com>
2023-06-19 12:56:47 +0100

    Dynamic Date added

--- src/transaction.js
+++ src/transaction.js
@@ -1,7 +1,15 @@
 class Transaction {
-  constructor(amount, date) {
+  constructor(amount) {
     this.amount = amount;
-    this.date = date;
+    this.date = this.formatDate(new Date());
+  }
+
+  formatDate(date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+
+    return `${day}-${month}-${year}`
   }
 }
 
--- test/transaction.test.js
+++ test/transaction.test.js
@@ -2,8 +2,10 @@ const Transaction = require('../src/transaction')
 
 describe ('Transaction', () => {
   it('should construct with an amount and a date', () => {
-    const trans = new Transaction(1500, '19-06-2023')
+    const trans = new Transaction(1500)
     expect(trans.amount).toEqual(1500)
     expect(trans.date).toEqual('19-06-2023')
+    // this test may break as it relies on the 
+    // current actual date
   })
 })
